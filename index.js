6ea3b401c3cc6c258016afdcceaeb0fd3a9a755c
nico <westernfarthing@gmail.com>
2023-01-25 23:39:05 -0600

    add user prompts

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
+require("dotenv").config();
+require("console.table");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-require("dotenv").config();
-require("console.table");
-
-// make connection to the database
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -14,7 +12,98 @@ const db = mysql.createConnection(
   },
   console.log("Connected to the database!")
 );
-// TODO: add user prompts
+
 function promptUser() {
-  inquirer.prompt([{}]);
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please select an option from the menu: ",
+        name: "options",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Quit",
+        ],
+      },
+    ])
+    .then(function (userInput) {
+      switch (userInput.options) {
+        case "View All Departments":
+          viewDepts();
+          break;
+        case "View All Roles":
+          viewRoles();
+          break;
+        case "View All Employees":
+          viewEmployees();
+          break;
+        case "Add a Department":
+          addDept();
+          break;
+        case "Add a Role":
+          addRole();
+          break;
+        case "Add an Employee":
+          addEmployee();
+          break;
+        case "Update an Employee Role":
+          updateRole();
+          break;
+        default:
+          exit();
+      }
+    });
 }
+
+function viewRoles() {
+  console.log("i view roles");
+  promptUser();
+}
+
+function viewDepts() {
+  console.log("i view departments");
+
+  promptUser();
+}
+
+function viewEmployees() {
+  console.log("I view employees ");
+
+  promptUser();
+}
+
+function addDept() {
+  console.log("I add a department ");
+
+  promptUser();
+}
+
+function addRole() {
+  console.log("I add a role ");
+
+  promptUser();
+}
+
+function addEmployee() {
+  console.log("I add an employee ");
+
+  promptUser();
+}
+
+function updateRole() {
+  console.log("I update a role ");
+
+  promptUser();
+}
+
+function exit() {
+  db.end();
+}
+
+promptUser();
